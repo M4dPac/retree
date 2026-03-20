e2be74cd4e11623f1a5ce5acc344a274d8a67d75
M4dPac <pac8891@gmail.com>
2026-03-20 19:38:31 +0300

    chore(build): add separate rtree_perf_xlarge bench binary for 1M files

---
+++ benches/rtree_perf_xlarge.rs
@@ -0,0 +1,29 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use std::time::Duration;
+
+#[path = "common/mod.rs"]
+mod common;
+
+use common::{run_rtree, tree_1m};
+
+fn bench_xlarge_1m(c: &mut Criterion) {
+    let t = tree_1m();
+
+    let mut group = c.benchmark_group("xlarge_1m_files");
+    group.sample_size(10);
+    group.measurement_time(Duration::from_secs(120));
+    group.warm_up_time(Duration::from_secs(10));
+
+    group.bench_function("seq_plain", |b| {
+        b.iter(|| run_rtree(&t.path, &[]));
+    });
+
+    group.bench_function("par_auto", |b| {
+        b.iter(|| run_rtree(&t.path, &["--parallel"]));
+    });
+
+    group.finish();
+}
+
+criterion_group!(benches, bench_xlarge_1m);
+criterion_main!(benches);
